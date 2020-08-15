4421c016599bbfd9e5a50b6a96bfd08cc6fc484b
Melanie <melanie-ponce@hotmail.com>
2020-08-15 00:06:36 -0400

    add landing page

---
+++ reactportfolio/src/components/index/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import '../styles/index.css'
+
+function Index(){
+    return (
+    <div className="indexBkg">
+        <div className="indexBody">
+            <div className="wrapper">
+                <div className="typing-demo">
+                    <h1>Hello World - My name is Melanie Ponce.</h1>
+                </div>
+            <button onClick="#';" className="noselect"></button>
+            </div>
+        </div>
+    </div>
+    )
+}
+
+export default Index;
