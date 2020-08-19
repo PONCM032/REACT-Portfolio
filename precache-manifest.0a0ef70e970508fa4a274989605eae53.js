339b34bdd855999e59adcaae6ad9ff2a401dd94b
Melanie <melanie-ponce@hotmail.com>
2020-08-18 22:13:19 -0400

    Updates

---
+++ precache-manifest.0a0ef70e970508fa4a274989605eae53.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0acb97f8cbf5675abd1687fcf5889727",
+    "url": "/REACT-Portfolio/index.html"
+  },
+  {
+    "revision": "01cc9bd96c1fa8aebf10",
+    "url": "/REACT-Portfolio/static/css/main.38fb628a.chunk.css"
+  },
+  {
+    "revision": "0a018e47668ac3baa08f",
+    "url": "/REACT-Portfolio/static/js/2.52a6e238.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/REACT-Portfolio/static/js/2.52a6e238.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01cc9bd96c1fa8aebf10",
+    "url": "/REACT-Portfolio/static/js/main.a48ce3d5.chunk.js"
+  },
+  {
+    "revision": "c833f22bca200d5518e6",
+    "url": "/REACT-Portfolio/static/js/runtime-main.97369afb.js"
+  },
+  {
+    "revision": "f2cc73415daae319e4d605cc47bc7806",
+    "url": "/REACT-Portfolio/static/media/Brew4UHeader.f2cc7341.png"
+  },
+  {
+    "revision": "ab3975b0539ede117a41180ba5d31dd3",
+    "url": "/REACT-Portfolio/static/media/PassGen.ab3975b0.png"
+  },
+  {
+    "revision": "2f7bc4841fb5150316771d0101d143b4",
+    "url": "/REACT-Portfolio/static/media/Quest_of_Corona.2f7bc484.png"
+  },
+  {
+    "revision": "bb188f96572833965365dd02b395b141",
+    "url": "/REACT-Portfolio/static/media/Quiz.bb188f96.png"
+  },
+  {
+    "revision": "48a5249c1e3588155f5e0058d5427d6f",
+    "url": "/REACT-Portfolio/static/media/ReadMe.48a5249c.png"
+  },
+  {
+    "revision": "fa32e8fbc99122e5816c253a6b6236ef",
+    "url": "/REACT-Portfolio/static/media/croppedProfileImage.fa32e8fb.JPG"
+  },
+  {
+    "revision": "7def0f92887913d2f44b9f5e2738b1df",
+    "url": "/REACT-Portfolio/static/media/mainPortfolioBkg.7def0f92.jpg"
+  }
+]);
